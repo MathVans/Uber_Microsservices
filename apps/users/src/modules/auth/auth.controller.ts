d328720bdf651c1146da3c28861dc97eb2abe1a7
Mateus Almeida <mateusalmeida0070@gmail.com>
2025-10-10 15:44:38 -0300

    feat: Creating user entity on database

--- apps/users/src/modules/auth/auth.controller.ts
+++ apps/users/src/modules/auth/auth.controller.ts
@@ -1,35 +1,8 @@
 import { Controller } from "@nestjs/common";
 import { MessagePattern, Payload } from "@nestjs/microservices";
 import { AuthService } from "./auth.service";
-import { CreateAuthDto } from "./dto/create-auth.dto";
-import { UpdateAuthDto } from "./dto/update-auth.dto";
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @MessagePattern("auth.createAuth")
-  create(@Payload() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @MessagePattern("auth.findAllAuth")
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @MessagePattern("auth.findOneAuth")
-  findOne(@Payload() id: number) {
-    return this.authService.findOne(id);
-  }
-
-  @MessagePattern("auth.updateAuth")
-  update(@Payload() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(updateAuthDto.id, updateAuthDto);
-  }
-
-  @MessagePattern("auth.removeAuth")
-  remove(@Payload() id: number) {
-    return this.authService.remove(id);
-  }
 }
