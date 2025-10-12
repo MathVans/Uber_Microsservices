7ab98255db747d87f50eb6f2aa0d43e9e3cf5aa8
MathVans <mateusalmeida0070@gmail.com>
2025-10-12 19:04:15 -0300

    feat: implementing the error exception for update user

--- apps/users/src/modules/users/entities/user.entity.ts
+++ apps/users/src/modules/users/entities/user.entity.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document, Types } from "mongoose";
 import * as bcrypt from "bcrypt";
-import { Role } from "apps/users/src/shared/common/enum/role.enum";
+import { Role } from "@app/common/shared/enum/role.enum";
 
 export type UserDocument = User & Document;
 
