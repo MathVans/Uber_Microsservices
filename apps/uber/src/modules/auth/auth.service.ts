ada3f7876fe9438d3a1335fa2f3a6b062c4b3092
MathVans <mateusalmeida0070@gmail.com>
2025-10-12 13:43:06 -0300

    feat: Implementing the login useCase

--- apps/uber/src/modules/auth/auth.service.ts
+++ apps/uber/src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { RegisterDto } from "./dto/register.dto";
 import { ClientProxy } from "@nestjs/microservices";
+import { LoginDto } from "./dto/login.dto";
 
 @Injectable()
 export class AuthService {
@@ -8,5 +9,9 @@ export class AuthService {
 
   async register(registerDto: RegisterDto) {
     return await this.clientProxy.send("auth.register", registerDto);
+  }
+
+  async login(loginDto: LoginDto) {
+    return await this.clientProxy.send("auth.login", loginDto);
   }
 }
