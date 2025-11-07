50e05ea39dd2cf097748e17b65c73d74074f775e
MateusAlmeida <mateusalmeida0070@gmail.com>
2025-11-07 16:29:44 -0300

    chore(dispatch-app): Creating the dispatch app and start configuring the consumer

--- apps/dispatch/src/app.module.ts
+++ apps/dispatch/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { MatchingController } from './modules/matching/matching.controller';
 import { MatchingService } from './modules/matching/matching.service';
 import { MatchingModule } from './modules/matching/matching.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MatchingModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath:"apps/dispatch/.env"
+  }),
+  MatchingModule
+],
   controllers: [MatchingController],
   providers: [MatchingService],
 })
--- apps/trip/src/shared/clients/clients.module.ts
+++ apps/trip/src/shared/clients/clients.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import {Partitioners} from 'kafkajs'
 
 @Module({
   imports: [
