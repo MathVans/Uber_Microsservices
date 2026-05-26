aa6c3f0652231e829b19a5649d8b2d47ca0dcc7b
MathVans <mateusalmeida0070@gmail.com>
2026-05-26 14:37:36 -0300

    chore(removing): Removing dtos and entity from dispatch

--- apps/dispatch/src/modules/matching/dto/update-matching.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMatchingDto } from './create-matching.dto';
-
-export class UpdateMatchingDto extends PartialType(CreateMatchingDto) {
-  id: number;
-}
